30cf2473a63b5cdc3056104ae958ad18e1658542
wangyongjia <1076006211@qq.com>
2021-05-25 14:56:34 +0800

    整体布局代码优化

---
+++ src/views/layout/components/slideNav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main-header">
+        <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+          <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <div class="breadcumb">
+            <a-breadcrumb>
+              <a-breadcrumb-item>Home</a-breadcrumb-item>
+              <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+            </a-breadcrumb>
+        </div>
+        <ul class="user-info">
+          <li>欢迎qiqi
+            <a-icon type="down" />
+          </li>
+          <li>退出</li>
+        </ul>
+      </div>
+</template>
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
